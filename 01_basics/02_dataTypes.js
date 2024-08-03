1cc7ecc0b92ec9baec96c43a94b63c089f2b1d29
kartiksharmagit <122426467+iamKartikSharma@users.noreply.github.com>
2024-08-03 05:07:30 +0000

    DataTypes Intro

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //Treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+let name = "Kartik"
+let age = 20
+let isLoggedIn = false
+
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "kartik")
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
