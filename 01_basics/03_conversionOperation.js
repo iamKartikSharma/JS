d15526d6580a28232c0b68b20166aeefc61aa4f4
kartiksharmagit <122426467+iamKartikSharma@users.noreply.github.com>
2024-08-03 05:43:16 +0000

    more operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,28 +1,59 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false => 0
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1; false => 0
 
-let isLoggedIn = "kartik"
+// let isLoggedIn = "kartik"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-// 1 => true; 0 => false
-// "" => false
-// "kartik" => true
+// // 1 => true; 0 => false
+// // "" => false
+// // "kartik" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
+// *************************** Operations *****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " kartik"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2) 
+// console.log(1 + 2 + "2")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter =  100
+gamecounter++
+
+console.log(gamecounter)
