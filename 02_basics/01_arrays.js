79881ac5ba6f08593b15b68a5e2d040bc0f2cfd1
kartiksharmagit <122426467+iamKartikSharma@users.noreply.github.com>
2024-08-11 08:43:52 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,31 @@
+//  array
+
+const myArr =[0, 1, 2, 3, 4, 5]
+const myHeroes = ["kartik", "ananya"]
+
+// const myArr2 = new Array(1, 2, 3,4)
+
+// console.log(myArr[0]);
+
+// Array methods
+
+myArr.push(6)
+// console.log(myArr);
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3))
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(myn2)
+
+
+console.log(myArr);
