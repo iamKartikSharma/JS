ee058daac32f75413913ef2c666be0a9aa67ced6
kartiksharmagit <122426467+iamKartikSharma@users.noreply.github.com>
2024-08-12 14:28:38 +0000

    Json Talks

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -43,8 +43,29 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.entries(tinderUser))
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course ={
+    coursename: "js in hindi",
+    price:"999",
+    courseInstructor:"kartik"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "kartik",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+
