5d96b258adfcfe4c972a5e7468ef43b5a0d62b3c
kartiksharmagit <122426467+iamKartikSharma@users.noreply.github.com>
2024-08-16 18:18:29 +0000

    Function with object and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -23,11 +23,32 @@ const result = addtwoNumbers(3,5) //arguments
 
 function loginuserMessage(username){
     if(!username){
-        console.log("please enter a username");
+        // console.log("please enter a username");
         return
     }
-    return `${username} just logged in`
+    // return `${username} just logged in`
 }
 
 // console.log(loginuserMessage("kartik"))
 console.log(loginuserMessage())
+
+function calculateCartPrice(...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200,300,400))
+
+const user = {
+    username: "kartik",
+    price:299
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username}and price is ${anyobject.price}`)
+}
+handleObject(user)
+
+const mynewArray = [200, 400 , 500, 700]
+
+function returnSecondvalue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondvalue(mynewArray))
